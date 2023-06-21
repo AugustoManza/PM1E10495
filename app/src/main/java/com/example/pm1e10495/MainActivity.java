29951a5b79e4b17d03556d5f091246c71927cc9a
Augusto Manzanarez <franciscomanzanarez1996@gmail.com>
2023-06-21 00:46:29 -0600

    21/06/2023 00:47

--- app/src/main/java/com/example/pm1e10495/MainActivity.java
+++ app/src/main/java/com/example/pm1e10495/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.pm1e10495;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner Lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Lista=(Spinner) findViewById(R.id.selectpais);
+
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.list, android.R.layout.simple_list_item_1);
+        Lista.setAdapter(adapter);
     }
 }
