cb5dd5d878f78a8d95d8320a2e5e778f9a61db50
Ashly <ashlyalva@gmail.com>
2023-06-21 22:42:49 -0600

    cambio 1 ashly

---
+++ app/src/main/java/com/example/pm1e10495/ListActivity.java
@@ -0,0 +1,13 @@
+package com.example.pm1e10495;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
